2eebcf3dbd138b0617a0278ccbbafc6002b7b374
silvinaas <silvinaas4@gmail.com>
2021-09-04 01:09:14 +0000

    cambiar colores

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function getRandom(number) {
+    return Math.floor(Math.random() * number);
+  }
+  let valorAleatorio = numbers[getRandom(13)];
+  let paloAleatorio = palos[getRandom(4)];
+  console.log(valorAleatorio, paloAleatorio);
+
+  let palo1 = document.querySelector(".palo");
+  let numero = document.querySelector(".number");
+  let palo2 = document.querySelector(".palo2");
+
+  palo1.innerHTML = paloAleatorio;
+  palo2.innerHTML = paloAleatorio;
+  numero.innerHTML = valorAleatorio;
 };
